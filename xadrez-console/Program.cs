b14d55f903d818ccc692b7d25b8c036d4e2c3642
Henrique Toyoda <thenrique32@gmail.com>
2023-02-14 20:47:18 -0300

    Atribuindo movimento nas pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,8 +14,16 @@ namespace xadrez_console {
 					Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
 
+					Console.WriteLine();
 					Console.Write("Origem: ");
 					Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+					bool[,] posicocesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+					Console.Clear();
+					Tela.imprimirTabuleiro(partida.tab, posicocesPossiveis);
+
+					Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
