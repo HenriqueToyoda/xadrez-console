21cc16914a3f24fe9f2fe6bbae1ce0f01b1c0897
Henrique Toyoda <thenrique32@gmail.com>
2023-02-20 20:27:32 -0300

    Correção: mudança de nome de metodo

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -44,7 +44,7 @@ namespace tabuleiro
             return false;
         }
 
-        public bool podeMoverPeca(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
